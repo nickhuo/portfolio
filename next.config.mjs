38e1c022b58106873e2a0bfcd3ee549c21e893c2
NickHuo <nickhuo@donutbrowser.ai>
2025-08-03 18:41:48 -0500

    fix deploy issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,13 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  trailingSlash: true,  
+  images: {
+    unoptimized: true  
+  }
 };
 
 const withMDX = createMDX({
