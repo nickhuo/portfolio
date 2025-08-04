4a4a215203e7b96098ffa43cd4db4b957d7564bf
NickHuo <nickhuo@donutbrowser.ai>
2025-08-04 00:23:53 -0500

    Update layout styles to improve visual consistency by changing background colors for light and dark modes in the header component.

--- app/writing/layout.tsx
+++ app/writing/layout.tsx
@@ -8,7 +8,7 @@ export default function LayoutBlogPost({
 }) {
   return (
     <>
-      <div className="pointer-events-none fixed left-0 top-0 z-10 h-12 w-full bg-gray-100 to-transparent backdrop-blur-xl [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)] dark:bg-zinc-950" />
+      <div className="pointer-events-none fixed left-0 top-0 z-10 h-12 w-full bg-white to-transparent backdrop-blur-xl [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)] dark:bg-[#111110]" />
       <ScrollProgress
         className="fixed top-0 z-20 h-0.5 bg-gray-300 dark:bg-zinc-600"
         springOptions={{
